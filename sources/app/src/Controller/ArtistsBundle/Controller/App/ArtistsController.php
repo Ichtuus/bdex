29c52bf020e0767609547484260580842070ac37
Diarra Nicolas <nicolas.diarra@stellar.io>
2020-01-20 23:42:00 +0100

    Create route who get all datas into ArtistsController receveid from api call in vue

--- sources/app/src/Controller/ArtistsBundle/Controller/App/ArtistsController.php
+++ sources/app/src/Controller/ArtistsBundle/Controller/App/ArtistsController.php
@@ -1,8 +1,10 @@
 <?php
 
 namespace App\Controller\ArtistsBundle\Controller\App;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class ArtistsController extends AbstractController
 {
@@ -11,10 +13,21 @@ class ArtistsController extends AbstractController
      * @Route("/artists", name="artists_list", options={"expose": true})
      *
      */
-    public function listAction(
-
-    ) {
+    public function listAction() {
         return $this->render('artists/list.html.twig');
     }
 
+    /**
+     * @Route("/artists/all", name="artists_get_list", options={"expose": true})
+     * @param Request $request
+     *
+     * @return \Symfony\Component\HttpFoundation\JsonResponse
+     */
+    public function getListArtists(Request $request) {
+
+        $data = $request->getContent();
+        $data = json_decode($data, true);
+
+        return $this->json($data);
+    }
 }
