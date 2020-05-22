aff841aa13eaa30f6a1d019b15243f1f226ecd0a
Diarra Nicolas <nicolas.diarra@stellar.io>
2020-05-22 19:10:38 +0200

    feat(#8): Add admin route and update config
    fixup

---
+++ sources/app/src/AdminBundle/Controller/AdminController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\AdminBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AdminController extends AbstractController
+{
+    /**
+     * @Route("/admin", name="user_admin")
+     * @IsGranted("ROLE_ADMIN")
+     * @return Response
+     */
+    public function adminDashboard(): Response
+    {
+        if (!$this->get('security.authorization_checker')->isGranted('ROLE_ADMIN')) {
+            throw $this->createAccessDeniedException('GET OUT!');
+        }
+        return $this->render('base.html.twig', []);
+    }
+}
